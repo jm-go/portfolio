e5ca54aa7121f150915d8ca7559b1d818e13aa7d
Jowita Gogol <jmg88@bath.ac.uk>
2024-04-29 23:09:55 +0100

    Updated Navbar

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,7 +1,31 @@
+import { motion, transform } from "framer-motion";
+import { useState } from "react";
+
 export const Test = () => {
+  const [open, setOpen] = useState(false);
+
+  const variants = {
+    visible: (i) => ({
+      opacity: 1,
+      x: 100,
+      transition: { delay: i * 0.3 },
+    }),
+    hidden: { opacity: 0 },
+  };
+
+  const items = ["item1", "item2", "item3", "item4"];
+
   return (
     <div className="course">
-        <div className="box"></div>
+      <motion.ul initial="hidden" animate="visible" variants={variants}>
+        {items.map((item, i) => {
+          <motion.li variants={variants} key={item} custom={i}>
+            {item}
+          </motion.li>;
+        })}
+      </motion.ul>
     </div>
-  )
-}
+  );
+};
+
+export default Test;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,35 @@
-import "./navbar.scss";
+import "./Navbar.scss";
+import { motion } from "framer-motion";
 
 export const Navbar = () => {
   return (
     <div className="navbar">
-        {/* Sidebar */}
-        <div className="wrapper">
-            <span>Jowita Gogol</span>
-            <div className="social">
-               <a href="#"><img src="/facebook.png" alt="" /></a> 
-               <a href="#"><img src="/instagram.png" alt="" /></a> 
-               <a href="#"><img src="/youtube.png" alt="" /></a> 
-               <a href="#"><img src="/dribble.png" alt="" /></a> 
-            </div>
+      <Sidebar/>
+      <div className="wrapper">
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          Jowita Gogol
+        </motion.span>
+        <div className="social">
+          <a href="#">
+            <img src="/facebook.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/instagram.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/youtube.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/dribble.png" alt="" />
+          </a>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default Navbar;
