3fca98231e8d61eb731476c0f9d81479dbdadffa
Jowita Gogol <jowita.m.gogol@gmail.com>
2024-04-30 00:09:10 +0100

    Updated Links component

--- src/components/sidebar/links/Links.jsx
+++ src/components/sidebar/links/Links.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 export const Links = () => {
+  const items = ["Homepage", "Services", "Portfolio", "Contact", "About"];
+
   return (
-    <div className="links">Links</div>
-  )
-}
+    <div className="links">
+      {items.map((item) => (
+        <a href={`#${item}`} key={item}>
+          {item}
+        </a>
+      ))}
+    </div>
+  );
+};
 
 export default Links;
